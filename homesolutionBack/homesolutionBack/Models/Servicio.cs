20c6c28d1f058d8c5e8392613d45be30093e3ca2
AmandaCode1 <amandacode1337@gmail.com>
2024-05-07 20:41:43 +0200

    conexion a base de datos

---
+++ homesolutionBack/homesolutionBack/Models/Servicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homesolutionBack.Models;
+
+public partial class Servicio
+{
+    public int ServicioId { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public decimal? Precio { get; set; }
+
+    public int? DuracionEstimada { get; set; }
+
+    public string? Categoria { get; set; }
+
+    public virtual ICollection<SolicitudServicio> SolicitudServicios { get; set; } = new List<SolicitudServicio>();
+}
---
+++ homesolutionBack/homesolutionBack/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homesolutionBack.Models;
+
+public partial class Usuario
+{
+    public int UserId { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? CorreoElectronico { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Rol { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public virtual ICollection<SolicitudServicio> SolicitudServicios { get; set; } = new List<SolicitudServicio>();
+}
